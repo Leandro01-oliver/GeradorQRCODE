0067c58be7b8c852873673705586336ac4573193
Leandro <leandro.oliver_lima@hotmail.com>
2023-02-17 18:12:55 -0300

    fix: Inclusão de title e icon do site

--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+      <link rel="icon " href='/qrcode.jpg' />
+      <title>
+        Leitor de QrCode em Texto
+      </title>
       <Head />
       <body>
         <Main />
