7d03dfbea6fca83d9a01e5a812be7e469e287621
Leandro <leandro.oliver_lima@hotmail.com>
2023-02-17 18:08:16 -0300

    fix:Correção de heigth e width

--- components/Home/Content/LeitorQRCODE/index.js
+++ components/Home/Content/LeitorQRCODE/index.js
@@ -1,23 +1,31 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Flex } from "@chakra-ui/react"
 import { useContext } from "react";
 import QRCode from "react-qr-code"
 import { GlobalProvider } from "../../../../context/GlobalContext";
 
-const LeitorQRODE = () =>{
+const LeitorQRODE = () => {
 
     const {
         search
-    } =  useContext(GlobalProvider);
+    } = useContext(GlobalProvider);
 
-    return(
+    return (
         <>
-            <QRCode 
-                value={search}
-                style={{
-                    width:'325px',
-                    height:'325px'
-                }}
+            <Flex
+                bg={'#fff'}
+                w={'320px'}
+                h={'320px'}
+                justifyContent={'center'}
+                alignItems={'center'}
+            >
+                <QRCode
+                    value={search}
+                    style={{
+                        width: '290px',
+                        height: '290px'
+                    }}
                 />
+            </Flex>
         </>
     )
 }
