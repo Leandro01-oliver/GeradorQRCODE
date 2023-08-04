97eab2464442397e87e68afd8ea15164bfc7cf1a
Leo <leandro.oliver_lima@hotmail.com>
2023-08-04 19:17:53 -0400

    Utilização de stream do node para fazer o downlaod

--- pages/api/qrcode/download/index.js
+++ pages/api/qrcode/download/index.js
@@ -1,33 +1,44 @@
 import qr from 'qrcode';
 import { randomUUID } from 'crypto';
+import fs from 'fs';
+import os from 'os';
+import path from 'path';
 
-const handlerDowloadQRCODE = (req, res) => {
-
-    const {
-        textQr
-    } = req.body;
-
+const handlerDownloadQRCode = async (req, res) => {
+    const { textQr } = req.body;
 
     if (req.method == "POST") {
-
-        qr.toFile(
-            `../../${randomUUID()}.png`,
-            textQr,
-            { width: '500' },
-            (err, code) => {
-
-                if (err) {
-                    return err.message
-                }
-
+        const file = `${randomUUID()}.png`;
+        const filename = path.join(os.homedir(), 'Downloads', file);
+        try {
+            await new Promise((resolve, reject) => {
+                qr.toFileStream(fs.createWriteStream(filename), textQr, { width: 500 }, (err) => {
+                    if (err) {
+                        reject(err); // Pass the error to the promise rejection
+                    } else {
+                        resolve(); // Resolve the promise when successful
+                    }
+                });
             });
 
-         res.status(201).json({
-            type: "sucess",
-            result: "Download realizado com sucesso do QRCODE"
+            res.status(201).json({
+                type: "success",
+                result: "Download realizado com sucesso do QRCODE"
+            });
+        } catch (error) {
+            // Handle errors and send an appropriate response
+            console.error("Error generating QR Code:", error);
+            res.status(500).json({
+                type: "error",
+                result: "Erro ao gerar o QRCODE"
+            });
+        }
+    } else {
+        res.status(405).json({
+            type: "error",
+            result: "Método não permitido"
         });
     }
+};
 
-}
-
-export default handlerDowloadQRCODE;
+export default handlerDownloadQRCode;
